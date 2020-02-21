7460c9525d3f96dd37839eceee24fb2bd67c5cb6
yangjialong <yangjialong@thejoyrun.com>
2020-02-21 21:10:51 +0800

    [B] build static

---
+++ dist/qa/sw.js
@@ -0,0 +1,19 @@
+importScripts("/precache-manifest.136c3d0b0b0c00782402acb151b5899f.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+if (workbox) {
+  var precacheController = new workbox.precaching.PrecacheController();
+  console.log(`Yay! Workbox is loaded 🎉`);
+
+  // 预缓存静态资源
+  workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+
+  function match({ url, event, }) {
+  }
+  // 路由请求缓存
+  workbox.routing.registerRoute(
+    match,
+    new workbox.strategies.StaleWhileRevalidate()
+  );
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
